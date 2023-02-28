943612c4a40039b9479b6fae3d7e00323456d513
arvind79 <81362519+arvind79@users.noreply.github.com>
2023-03-01 01:22:56 +0530

    added features and design is changed
    added some extra buttons and changed the design of the website

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
+
 let count = 0
+let totalCount = 0
 
 function increment() {
     count += 1
     countEl.textContent = count
 }
 
+function decrement() {
+    count -= 1;
+    countEl.textContent = count
+}
+
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
+    totalCount += count
+    totalEl.textContent = totalCount
     countEl.textContent = 0
     count = 0
+}
+
+function reset() {
+    saveEl.textContent = "Previous entries: "
+    count = 0
+    totalEl.textContent = "Total Passengers: "
+    totalCount = 0
 }
